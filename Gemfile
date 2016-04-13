6eb4ab14559f72a996910d179154cd9c87b94c60
tuffylock <tuffylock@gmail.com>
2016-04-13 16:41:03 -0700

    add postgresql database

--- Gemfile
+++ Gemfile
@@ -2,3 +2,8 @@ source 'https://rubygems.org'
 ruby '2.1.2'
 
 gem 'sinatra'
+
+# database
+gem 'sinatra-activerecord'
+gem 'pg'
+gem 'rake'
--- rss.rb
+++ rss.rb
@@ -1,5 +1,8 @@
 require 'sinatra'
+require 'sinatra/activerecord'
 
 get '/' do
+  db_time = database.connection.execute('SELECT CURRENT_TIMESTAMP').first['now']
+  request.logger.info "DB time is #{db_time}"
   erb :hello_world
 end
